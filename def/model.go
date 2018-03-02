855e01a5a7e4a85b6c425ee827a354d7c88b2c52
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-03-02 08:45:57 -0300

    starting yaml

--- def/model.go
+++ def/model.go
@@ -1,23 +1,30 @@
 package def
 
-// Container structed
+// Container definitions
 type Container struct {
+	// Container struct metadata
+	Name string
+	Docs string
+
 	// Packages that will be used and their aliases (when needed)
-	Packages map[string]*string
+	Packages []Package
 
 	// Services to be managed by the container
 	Services map[string]Service
+}
 
-	// Services aliases
-	Aliases map[string]string
+// Package represents a package in the Container
+type Package struct {
+	Package string
+	Alias   *string
 }
 
 // Service represents a entry in the services config
 type Service struct {
 	Factory   *string
-	Arguments *[]interface{}
+	Arguments *[]Value
 	Struct    *string
-	InitMap   *map[string]interface{}
+	Fields    *map[string]Value
 }
 
 // IsByFactory when a factory function factory should be called
@@ -28,4 +35,49 @@ func (s Service) IsByFactory() bool {
 // IsByInitialization when the struct should be initialized
 func (s Service) IsByInitialization() bool {
 	return s.Struct != nil
+}
+
+// ValueType represents the type of the value
+type ValueType int16
+
+const (
+	// ValueSingle is a value that is static and single valued
+	ValueSingle ValueType = iota << 1
+	// ValueStruct is a map or struct of values
+	ValueStruct
+	// ValueSlice is a slice of values
+	ValueSlice
+	// ValueService is a reference to a service
+	ValueService
+)
+
+// Value represents a generic value (may be a reference to a service, a static value or static struct)
+type Value struct {
+	value     interface{}
+	valueType ValueType
+}
+
+// ValueType represents the type of the value
+func (v Value) ValueType() ValueType {
+	return v.valueType
+}
+
+// GetSingleValue should be used to get the value of a ValueSingle type
+func (v Value) GetSingleValue() string {
+	return v.value.(string)
+}
+
+// GetStruct should be used to get the value of a ValueStruct type
+func (v Value) GetStruct() map[string]Value {
+	return v.value.(map[string]Value)
+}
+
+// GetSlice should be used to get the value of a ValueSlice type
+func (v Value) GetSlice() []Value {
+	return v.value.([]Value)
+}
+
+// GetService should be used to get the value of a ValueService type
+func (v Value) GetService() string {
+	return v.value.(string)
 }
---
+++ def/yaml.go
@@ -0,0 +1,63 @@
+package def
+
+import (
+	"fmt"
+	"strings"
+)
+
+// UnmarshalYAML fot the Package
+func (p *Package) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	defaultErr := fmt.Errorf("the package should a single strig or a map of strings with a element")
+
+	var aliasedPackage map[string]string
+	if err := unmarshal(&aliasedPackage); err == nil {
+		if len(aliasedPackage) != 1 {
+			return defaultErr
+		}
+		for packageStr, alias := range aliasedPackage {
+			p.Package = packageStr
+			p.Alias = &alias
+			return nil
+		}
+	}
+
+	var packageName string
+	if err := unmarshal(&packageName); err == nil {
+		p.Package = packageName
+		return nil
+	}
+
+	return defaultErr
+}
+
+// UnmarshalYAML fot the Value
+func (v *Value) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var structValue map[string]Value
+	if err := unmarshal(&structValue); err == nil {
+		v.valueType = ValueStruct
+		v.value = structValue
+		return nil
+	}
+
+	var sliceValue []Value
+	if err := unmarshal(&sliceValue); err == nil {
+		v.valueType = ValueSlice
+		v.value = sliceValue
+		return nil
+	}
+
+	var value string
+	if err := unmarshal(&value); err == nil {
+		if strings.HasPrefix(value, "@") {
+			v.valueType = ValueService
+			v.value = value[:1]
+			return nil
+		}
+
+		v.valueType = ValueSlice
+		v.value = value
+		return nil
+	}
+
+	return fmt.Errorf("the value type was not recognized")
+}
