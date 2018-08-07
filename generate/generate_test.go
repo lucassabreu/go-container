c088a2d40eb29d0b2a2bfd89cf8cd08f2af1b3e2
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-08-06 21:43:29 -0300

    inside

--- generate/generate_test.go
+++ generate/generate_test.go
@@ -1,7 +1,6 @@
 package generate_test
 
 import (
-	"io/ioutil"
 	"regexp"
 	"testing"
 
@@ -17,12 +16,70 @@ func TestCircularReference(t *testing.T) {
 		Err       *string
 	}
 
-	bytes, err := ioutil.ReadFile("./test-files/circular-reference.yml")
-	if err != nil {
-		t.Error(err)
-	}
+	bytes := []byte(`
+- err: "Service Dependency not found"
+  container:
+    services:
+      Dependent:
+        factory: test.NewDependent
+        arguments:
+          - "@Dependency"
 
-	err = yaml.Unmarshal(bytes, &tests)
+- err: "Service OtherDependency not found"
+  container:
+    services:
+      Dependent:
+        struct: test.Dependent
+        fields:
+          OtherDependency: "@OtherDependency"
+
+- err: "There is a circular reference for @Service\\\\w -> @Service\\\\w -> @Service\\\\w"
+  container:
+    services:
+      Service1:
+        factory: test.NewService
+        arguments:
+          - "@Service2"
+      Service2:
+        factory: test.NewService
+        arguments:
+          - "@Service1"
+
+- err: "There is a circular reference for @\\\\w* -> @\\\\w* -> @\\\\w* -> @\\\\w*"
+  container:
+    services:
+      Factory:
+        factory: test.NewService
+        arguments:
+          - Service: "@Struct"
+      Struct2:
+        struct: test.Service
+      Factory2:
+        factory: test.Service
+      MiddleOne:
+        factory: test.NewService
+        arguments:
+          - "@Factory"
+      Struct:
+        struct: test.Service
+        fields:
+          Services:
+            - "@Struct2"
+            - "@Factory2"
+            - "@MiddleOne"
+
+- container:
+  services:
+    Factory:
+      factory: test.NewService
+      arguments: [ "@Struct" ]
+    Struct:
+      struct: test.Service
+      fields:
+        - Service: "@Factory"
+`)
+
+	err := yaml.Unmarshal(bytes, &tests)
 	if err != nil {
 		t.Error(err, ":", string(bytes))
 	}
