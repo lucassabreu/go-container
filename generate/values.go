5bf3a7e73cee356f3fa4e8dee511955a2c47d41f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-08-30 08:54:36 -0300

    sliceValue

--- generate/values.go
+++ generate/values.go
@@ -1,6 +1,7 @@
 package generate
 
 import (
+	"bytes"
 	"fmt"
 	"go/types"
 	"strconv"
@@ -11,16 +12,32 @@ import (
 func (cg ContainerGenerator) createValue(t types.Type, arg def.Value) (v Value, err error) {
 	switch arg.ValueType() {
 	case def.ValueSingle:
-
 		v = &ConstantValue{
 			value: arg.GetSingleValue(),
 			typ:   t,
 		}
 		return
+	case def.ValueService:
+		v = &ServiceReference{
+			ServiceName: arg.GetService(),
+			Type:        t,
+		}
+		return
+	case def.ValueSlice:
+		v = &SliceValue{
+			Value: arg,
+			Type:  t,
+		}
+		return
+	case def.ValueStruct:
+		v = &StructValue{
+			Value: arg,
+			Type:  t,
+		}
+		return
 	default:
 		return nil, fmt.Errorf("Value type %s was not recognized", arg.ValueType().String())
 	}
-
 }
 
 // Value will generate a value definition or use
@@ -54,6 +71,7 @@ func constantValueToString(v string, kind types.BasicKind) string {
 	}
 	return v
 }
+
 func (v *ConstantValue) GenerateVariable(varName string) string {
 	if !v.NeedsVariable() {
 		return ""
@@ -63,6 +81,7 @@ func (v *ConstantValue) GenerateVariable(varName string) string {
 	v.varName = varName
 	return fmt.Sprintf("%s := %s", varName, constantValueToString(v.value, typ.Kind()))
 }
+
 func (v *ConstantValue) GenerateUse() string {
 	if !v.NeedsVariable() {
 		typ := v.typ.(*types.Basic)
@@ -71,14 +90,115 @@ func (v *ConstantValue) GenerateUse() string {
 	return fmt.Sprintf("&%s", v.varName)
 }
 
-type slideValue struct {
-	values []Value
+type ServiceReference struct {
+	Type        types.Type
+	ServiceName string
 }
 
-func (slideValue) Generate(ContainerGenerator) string {
-	panic("not implemented")
+func (v *ServiceReference) Build(c *ContainerGenerator) {
 }
 
-func (slideValue) NeedsPointer() bool {
-	panic("not implemented")
+func (v *ServiceReference) NeedsVariable() bool {
+	return false
+}
+
+func (v *ServiceReference) GenerateVariable(varName string) string {
+	return ""
+}
+
+func (v *ServiceReference) GenerateUse() string {
+	return fmt.Sprintf("c.Get%s()", v.ServiceName)
+}
+
+type SliceValue struct {
+	Value         def.Value
+	Type          types.Type
+	values        []Value
+	needsVariable bool
+	pointer       bool
+	sliceType     SliceType
+	varName       string
+}
+
+func (v *SliceValue) Build(c *ContainerGenerator) {
+	var t *types.Slice
+	_, ok := v.Type.(*types.Pointer)
+	v.pointer = ok
+	v.needsVariable = ok
+
+	if v.pointer {
+		t = v.Type.(*types.Pointer).Elem().(*types.Slice)
+	} else {
+		t = v.Type.(*types.Slice)
+	}
+
+	v.sliceType = c.RegisterType(t).(SliceType)
+
+	values := v.Value.GetSlice()
+	v.values = make([]Value, len(values))
+	for i, d := range values {
+		v.values[i], _ = c.createValue(t.Elem(), d)
+		v.values[i].Build(c)
+		v.needsVariable = v.needsVariable || v.values[i].NeedsVariable()
+	}
+}
+
+func (v *SliceValue) NeedsVariable() bool {
+	return v.needsVariable
+}
+
+func (v *SliceValue) GenerateVariable(varName string) string {
+	v.varName = varName
+	b := &bytes.Buffer{}
+	for i, value := range v.values {
+		b.WriteString(value.GenerateVariable(fmt.Sprintf("%s_%d", varName, i)))
+		b.WriteRune('\n')
+	}
+
+	if v.pointer {
+		b.WriteString(varName)
+		b.WriteString(" := ")
+		sliceValueToUseString(v, b)
+	}
+
+	return b.String()
+}
+
+func sliceValueToUseString(v *SliceValue, b *bytes.Buffer) {
+	b.WriteString(v.sliceType.String())
+	b.WriteString("{\n")
+	for _, value := range v.values {
+		b.WriteString(value.GenerateUse())
+		b.WriteString(",\n")
+	}
+	b.WriteRune('}')
+}
+
+func (v *SliceValue) GenerateUse() string {
+	if v.pointer {
+		return fmt.Sprintf("&%s", v.varName)
+	}
+	b := &bytes.Buffer{}
+	sliceValueToUseString(v, b)
+	return b.String()
+}
+
+type StructValue struct {
+	Value def.Value
+	Type  types.Type
+}
+
+func (v *StructValue) Build(c *ContainerGenerator) {
+}
+
+func (v *StructValue) NeedsVariable() bool {
+	return false
+}
+
+func (v *StructValue) GenerateVariable(varName string) string {
+	return ""
+}
+
+func (v *StructValue) GenerateUse() string {
+	return fmt.Sprintf("%#v", v.Value)
 }
